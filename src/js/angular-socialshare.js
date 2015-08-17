55b9cfda89833cfc818015c5fb943f3a74f7523c
45kb <filippo.oretti@gmail.com>
2015-08-17 12:19:20 +0200

    fixed tumblr share, no need for type on it, if media is present we switch sharer url only @TheWrongAlice :)

--- src/js/angular-socialshare.js
+++ src/js/angular-socialshare.js
@@ -5,11 +5,12 @@
   'use strict';
 
   angular.module('720kb.socialshare', [])
-  .directive('socialshare', ['$window', '$location', function manageDirective ($window, $location) {
+  .directive('socialshare', ['$window', '$location',
+  function manageDirective($window, $location) {
 
     return {
       'restrict': 'A',
-      'link': function linkingFunction ($scope, element, attr) {
+      'link': function linkingFunction($scope, element, attr) {
 
         var key,
           attributeName,
@@ -29,31 +30,31 @@
           'popupWidth': 500
         };
 
-        // Observe the values in each of the properties so that if they're updated elsewhere,
+        // observe the values in each of the properties so that if they're updated elsewhere,
         // they are updated in this directive.
         for (key in propDefaults) {
           if (propDefaults.hasOwnProperty(key)) {
             attributeName = 'socialshare' + key.substring(0, 1).toUpperCase() + key.substring(1);
-              (function (keyName) {
-                attr.$observe(attributeName, function (value) {
-                  if (value) {
-                    properties[keyName] = value;
-                  }
-                });
-              }(key));
-              if (properties[key] === undefined){
-                  properties[key] = propDefaults[key];
-              }
+            (function observePropValues(keyName) {
+              attr.$observe(attributeName, function observeAttrs(value) {
+                if (value) {
+                  properties[keyName] = value;
+                }
+              });
+            }(key));
+            if (properties[key] === undefined){
+              properties[key] = propDefaults[key];
+            }
           }
         }
 
         properties.eventTrigger = attr.socialshareTrigger || 'click';
 
-        $scope.facebookShare = function manageFacebookShare (data) {
+        $scope.facebookShare = function manageFacebookShare(data) {
 
           if (data.type && data.type === 'feed') {
 
-            // If user specifies that they want to use the Facebook feed dialog (https://developers.facebook.com/docs/sharing/reference/feed-dialog/v2.4)
+            // if user specifies that they want to use the Facebook feed dialog (https://developers.facebook.com/docs/sharing/reference/feed-dialog/v2.4)
             var urlString = 'https://www.facebook.com/dialog/feed?display=popup' +
               '&app_id=' + encodeURI(data.via) +
               '&redirect_uri=' + encodeURI(data.redirectUri);
@@ -80,14 +81,14 @@
 
           } else {
 
-            // Otherwise default to using sharer.php
+            //otherwise default to using sharer.php
             $window.open(
               '//www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(data.url || $location.absUrl())
               , 'sharer', 'toolbar=0,status=0,width=' + data.popupWidth + ',height=' + data.popupHeight);
           }
         };
 
-        $scope.twitterShare = function manageTwitterShare (data) {
+        $scope.twitterShare = function manageTwitterShare(data) {
           var urlString = '//www.twitter.com/intent/tweet?';
 
           if (data.text) {
@@ -102,7 +103,7 @@
             urlString += '&hashtags=' + encodeURI(data.hashtags);
           }
 
-          // Default to the current page if a URL isn't specified
+          //default to the current page if a URL isn't specified
           urlString += '&url=' + encodeURIComponent(data.url || $location.absUrl());
 
           $window.open(
@@ -112,14 +113,14 @@
 
         };
 
-        $scope.googlePlusShare = function manageGooglePlusShare (data) {
+        $scope.googlePlusShare = function manageGooglePlusShare(data) {
 
           $window.open(
             '//plus.google.com/share?url=' + encodeURIComponent(data.url || $location.absUrl())
             , 'sharer', 'toolbar=0,status=0,width=' + data.popupWidth + ',height=' + data.popupHeight);
         };
 
-        $scope.redditShare = function manageRedditShare (data) {
+        $scope.redditShare = function manageRedditShare(data) {
           var urlString = '//www.reddit.com/';
 
           if (data.subreddit) {
@@ -128,7 +129,7 @@
             urlString += 'submit?url=';
           }
 
-          /*
+          /*-
            * Reddit isn't responsive and at default width for our popups (500 x 500), everything is messed up.
            * So, overriding the width if it is less than 900 (played around to settle on this) and height if
            * it is less than 650px.
@@ -146,49 +147,46 @@
             , 'sharer', 'toolbar=0,status=0,width=' + data.popupWidth + ',height=' + data.popupHeight);
         };
 
-        $scope.stumbleuponShare = function manageStumbleuponShare (data) {
+        $scope.stumbleuponShare = function manageStumbleuponShare(data) {
 
           $window.open(
             '//www.stumbleupon.com/submit?url=' + encodeURIComponent(data.url || $location.absUrl()) + '&title=' + encodeURI(data.text)
             , 'sharer', 'toolbar=0,status=0,width=' + data.popupWidth + ',height=' + data.popupHeight);
         };
 
-        $scope.linkedinShare = function manageLinkedinShare (data) {
+        $scope.linkedinShare = function manageLinkedinShare(data) {
 
           $window.open(
             '//www.linkedin.com/shareArticle?mini=true&url=' + encodeURIComponent(data.url || $location.absUrl()) + '&title=' + encodeURI(data.text)
             , 'sharer', 'toolbar=0,status=0,width=' + data.popupWidth + ',height=' + data.popupHeight);
         };
 
-        $scope.pinterestShare = function managePinterestShare (data) {
+        $scope.pinterestShare = function managePinterestShare(data) {
 
           $window.open(
             '//www.pinterest.com/pin/create/button/?url=' + encodeURIComponent(data.url || $location.absUrl()) + '&media=' + encodeURI(data.media) + '&description=' + encodeURI(data.text)
             , 'sharer', 'toolbar=0,status=0,width=' + data.popupWidth + ',height=' + data.popupHeight);
         };
 
-        $scope.diggShare = function manageDiggShare (data) {
+        $scope.diggShare = function manageDiggShare(data) {
 
           $window.open(
             '//www.digg.com/submit?url=' + encodeURIComponent(data.url || $location.absUrl()) + '&title=' + encodeURI(data.text)
             , 'sharer', 'toolbar=0,status=0,width=' + data.popupWidth + ',height=' + data.popupHeight);
         };
 
-        $scope.tumblrShare = function manageTumblrShare (data) {
+        $scope.tumblrShare = function manageTumblrShare(data) {
 
-          if (data.type && data.type === 'photo') {
+          if (data.media) {
             var urlString = '//www.tumblr.com/share/photo?source=' + encodeURIComponent(data.media);
 
             if (data.text) {
               urlString += '&caption=' + encodeURI(data.text);
-            } else if (data.caption) {
-              urlString += '&caption=' + encodeURI(data.caption);
             }
 
             $window.open(
                 urlString,
                 'sharer', 'toolbar=0,status=0,width=' + data.popupWidth + ',height=' + data.popupHeight);
-
           } else {
 
             $window.open(
@@ -197,21 +195,21 @@
           }
         };
 
-        $scope.vkShare = function manageVkShare (data) {
+        $scope.vkShare = function manageVkShare(data) {
 
-         $window.open(
+          $window.open(
             '//www.vk.com/share.php?url=' + encodeURIComponent(data.url || $location.absUrl())
             , 'sharer', 'toolbar=0,status=0,width=' + data.popupWidth + ',height=' + data.popupHeight);
         };
 
-        $scope.deliciousShare = function manageDeliciousShare (data) {
+        $scope.deliciousShare = function manageDeliciousShare(data) {
 
           $window.open(
             '//www.delicious.com/save?v=5&noui&jump=close&url=' + encodeURIComponent(data.url || $location.absUrl()) + '&title=' + encodeURI(data.text)
             , 'sharer', 'toolbar=0,status=0,width=' + data.popupWidth + ',height=' + data.popupHeight);
         };
 
-        $scope.bufferShare = function manageBufferShare (data) {
+        $scope.bufferShare = function manageBufferShare(data) {
           var urlString = '//bufferapp.com/add?';
 
           if (data.text) {
@@ -222,7 +220,7 @@
             urlString += '&via=' + encodeURI(data.via);
           }
 
-          // Default to the current page if a URL isn't specified
+          //default to the current page if a URL isn't specified
           urlString += '&url=' + encodeURIComponent(data.url || $location.absUrl());
 
           $window.open(
