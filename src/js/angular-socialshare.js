6128aa318d9d2bcd97634201351ce285469aa95e
45kb <filippo.oretti@gmail.com>
2015-08-19 08:27:21 +0200

    Fixed absolute urls for different protocols like file://

--- src/js/angular-socialshare.js
+++ src/js/angular-socialshare.js
@@ -12,30 +12,29 @@
       'restrict': 'A',
       'link': function linkingFunction($scope, element, attr) {
 
-        var key,
-          attributeName,
-          properties = {},
-          propDefaults = {
-          'url': '',
-          'redirectUri': '',
-          'provider': '',
-          'type': '',
-          'text': '',
-          'caption': '',
-          'to': '',
-          'ref': '',
-          'display': '',
-          'from': '',
-          'media': '',
-          'hashtags': '',
-          'via': '',
-          'description': '',
-          'source': '',
-          'subreddit': '',
-          'popupHeight': 500,
-          'popupWidth': 500
-        };
-
+        var key
+          , attributeName
+          , properties = {}
+          , propDefaults = {
+            'url': '',
+            'redirectUri': '',
+            'provider': '',
+            'type': '',
+            'text': '',
+            'caption': '',
+            'to': '',
+            'ref': '',
+            'display': '',
+            'from': '',
+            'media': '',
+            'hashtags': '',
+            'via': '',
+            'description': '',
+            'source': '',
+            'subreddit': '',
+            'popupHeight': 500,
+            'popupWidth': 500
+          };
         // observe the values in each of the properties so that if they're updated elsewhere,
         // they are updated in this directive.
         for (key in propDefaults) {
@@ -112,13 +111,13 @@
 
             //otherwise default to using sharer.php
             $window.open(
-              '//www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(data.url || $location.absUrl())
+              'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(data.url || $location.absUrl())
               , 'sharer', 'toolbar=0,status=0,width=' + data.popupWidth + ',height=' + data.popupHeight);
           }
         };
 
         $scope.twitterShare = function manageTwitterShare(data) {
-          var urlString = '//www.twitter.com/intent/tweet?';
+          var urlString = 'https://www.twitter.com/intent/tweet?';
 
           if (data.text) {
             urlString += 'text=' + encodeURIComponent(data.text);
@@ -150,7 +149,7 @@
         };
 
         $scope.redditShare = function manageRedditShare(data) {
-          var urlString = '//www.reddit.com/';
+          var urlString = 'https://www.reddit.com/';
 
           if (data.subreddit) {
             urlString += 'r/' + data.subreddit + '/submit?url=';
@@ -179,35 +178,35 @@
         $scope.stumbleuponShare = function manageStumbleuponShare(data) {
 
           $window.open(
-            '//www.stumbleupon.com/submit?url=' + encodeURIComponent(data.url || $location.absUrl()) + '&title=' + encodeURI(data.text)
+            'https://www.stumbleupon.com/submit?url=' + encodeURIComponent(data.url || $location.absUrl()) + '&title=' + encodeURI(data.text)
             , 'sharer', 'toolbar=0,status=0,width=' + data.popupWidth + ',height=' + data.popupHeight);
         };
 
         $scope.linkedinShare = function manageLinkedinShare(data) {
 
           $window.open(
-            '//www.linkedin.com/shareArticle?mini=true&url=' + encodeURIComponent(data.url || $location.absUrl()) + '&title=' + encodeURI(data.text)
+            'https://www.linkedin.com/shareArticle?mini=true&url=' + encodeURIComponent(data.url || $location.absUrl()) + '&title=' + encodeURI(data.text)
             , 'sharer', 'toolbar=0,status=0,width=' + data.popupWidth + ',height=' + data.popupHeight);
         };
 
         $scope.pinterestShare = function managePinterestShare(data) {
 
           $window.open(
-            '//www.pinterest.com/pin/create/button/?url=' + encodeURIComponent(data.url || $location.absUrl()) + '&media=' + encodeURI(data.media) + '&description=' + encodeURI(data.text)
+            'https://www.pinterest.com/pin/create/button/?url=' + encodeURIComponent(data.url || $location.absUrl()) + '&media=' + encodeURI(data.media) + '&description=' + encodeURI(data.text)
             , 'sharer', 'toolbar=0,status=0,width=' + data.popupWidth + ',height=' + data.popupHeight);
         };
 
         $scope.diggShare = function manageDiggShare(data) {
 
           $window.open(
-            '//www.digg.com/submit?url=' + encodeURIComponent(data.url || $location.absUrl()) + '&title=' + encodeURI(data.text)
+            'https://www.digg.com/submit?url=' + encodeURIComponent(data.url || $location.absUrl()) + '&title=' + encodeURI(data.text)
             , 'sharer', 'toolbar=0,status=0,width=' + data.popupWidth + ',height=' + data.popupHeight);
         };
 
         $scope.tumblrShare = function manageTumblrShare(data) {
 
           if (data.media) {
-            var urlString = '//www.tumblr.com/share/photo?source=' + encodeURIComponent(data.media);
+            var urlString = 'https://www.tumblr.com/share/photo?source=' + encodeURIComponent(data.media);
 
             if (data.text) {
               urlString += '&caption=' + encodeURI(data.text);
@@ -219,7 +218,7 @@
           } else {
 
             $window.open(
-                '//www.tumblr.com/share/link?url=' + encodeURIComponent(data.url) + '&description=' + encodeURI(data.text)
+                'https://www.tumblr.com/share/link?url=' + encodeURIComponent(data.url) + '&description=' + encodeURI(data.text)
                 , 'sharer', 'toolbar=0,status=0,width=' + data.popupWidth + ',height=' + data.popupHeight);
           }
         };
@@ -227,14 +226,14 @@
         $scope.vkShare = function manageVkShare(data) {
 
           $window.open(
-            '//www.vk.com/share.php?url=' + encodeURIComponent(data.url || $location.absUrl())
+            'https://www.vk.com/share.php?url=' + encodeURIComponent(data.url || $location.absUrl())
             , 'sharer', 'toolbar=0,status=0,width=' + data.popupWidth + ',height=' + data.popupHeight);
         };
 
         $scope.deliciousShare = function manageDeliciousShare(data) {
 
           $window.open(
-            '//www.delicious.com/save?v=5&noui&jump=close&url=' + encodeURIComponent(data.url || $location.absUrl()) + '&title=' + encodeURI(data.text)
+            'https://www.delicious.com/save?v=5&noui&jump=close&url=' + encodeURIComponent(data.url || $location.absUrl()) + '&title=' + encodeURI(data.text)
             , 'sharer', 'toolbar=0,status=0,width=' + data.popupWidth + ',height=' + data.popupHeight);
         };
 
