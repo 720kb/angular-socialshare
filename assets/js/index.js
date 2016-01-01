15ee04dfdd009e82c25ef9eea24b97e6e0caeed9
45kb <filippo.oretti@gmail.com>
2016-01-01 22:14:04 +0100

    config provider to test

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,5 +6,20 @@
   angular.module('720kb', [
     'ngRoute',
     '720kb.socialshare'
-  ]);
+  ])
+  .config(['socialshareConfProvider', function configApp(socialshareConfProvider) {
+
+    socialshareConfProvider.configure([{
+      'provider': 'twitter',
+      'conf': {
+        'url': 'provider.com',
+        'text': 'provider',
+        'via': '',
+        'hashtags': '',
+        'trigger': 'click',
+        'popupHeight': 300,
+        'popupWidth' :400
+      }
+    }]);
+  }]);
 }(angular));
